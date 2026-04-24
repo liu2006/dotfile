88beb6969407d7e0fd9dc2fb7c48af303c45981a
liu2006 <2102033535@qq.com>
2026-04-24 10:18:28 +0800

    update

--- shaders/compile.sh
+++
@@ -1,8 +0,0 @@
-/home/liu/vendor/1.4.341.1/x86_64/bin/slangc shader.slang \
-                                             -target spirv \
-                                             -profile spirv_1_4 \
-                                             -emit-spirv-directly \
-                                             -fvk-use-entrypoint-name \
-                                             -entry vertMain \
-                                             -entry fragMain \
-                                             -o slang.spv
